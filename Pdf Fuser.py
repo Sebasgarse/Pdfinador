81f88b44d350147e5ddc6f621ceb48c49725227c
Sebastian Garcia <Sebasgarse@gmail.com>
2019-09-03 16:03:44 -0400

    version 1, con todo lo esperado

--- Pdf Fuser.py
+++ Pdf Fuser.py
@@ -1,4 +1,4 @@
-import os, PyPDF2, re
+import os, PyPDF2, re, shutil
 from PIL import Image
 
 def inicio():
@@ -8,17 +8,35 @@ def inicio():
         archivos = [n for n in os.listdir(carpeta) if n.endswith('.jpg') 
                                                     or n.endswith('.png') 
                                                     or n.endswith('.jpeg')]
+        archivos.sort()
                                                     
         os.chdir(carpeta)
         i = 0
         os.mkdir("pdf")
+        
+        writer = PyPDF2.PdfFileWriter()
+        
         formato = re.compile(r'.jpg|.png|.jpeg')
+        
+        files = []
         for imagen in archivos:
+            archivo = 'pdf\\' + formato.sub('', imagen) + '.pdf'
             im = Image.open(imagen)
-            im.save('pdf\\' + formato.sub('', imagen) + '.pdf', 'PDF', resoultion = 100.0)
+            im.save(archivo, 'PDF', resoultion = 100.0)
+            pdf = open(archivo, 'rb')
+            files.append(pdf)
+            reader = PyPDF2.PdfFileReader(pdf)
+            for pagina in range(reader.numPages):
+                page = reader.getPage(pagina)
+                writer.addPage(page)
             i += 1
             print('Completado ' + str(int(100 * (i / len(archivos)))) + '% \r',end='',flush=True)
-        
+        final = open(os.path.basename(carpeta) + '.pdf', 'wb')
+        writer.write(final)
+        final.close()
+        for file in files:
+            file.close()
+        shutil.rmtree("pdf")
         
     else:
         print('Carpeta no existe')
