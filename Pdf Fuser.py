7591d96a98cbfdd039bd67f603e577c7962ea8b8
Sebastian Garcia <Sebasgarse@gmail.com>
2019-09-03 02:02:39 -0400

    Mejore el codigo para que los margenes no estorbaran

--- Pdf Fuser.py
+++ Pdf Fuser.py
@@ -1,5 +1,6 @@
 import os, PyPDF2, docx
 from PIL import Image
+from lxml import etree
 
 def inicio():
     print("Escriba la direccion de la carpeta")
@@ -17,13 +18,20 @@ def inicio():
             doc.add_picture(imagen, width=docx.shared.Inches(8.5), height=docx.shared.Inches(11))
             i += 1
             print('Completado ' + str(int(100 * (i / len(archivos)))) + '% \r',end='',flush=True)
-            
+        
+        sections = doc.sections
+        for section in sections:
+            section.bottom_margin = 0
+            section.left_margin = 0
+            section.right_margin = 0
+            section.top_margin = 0
+
         doc.save(os.path.basename(carpeta) + '.docx')
         
     else:
         print('Carpeta no existe')
         inicio()
         
-        
+
 if __name__ == '__main__':
     inicio()
