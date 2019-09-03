810ed246eeafc39b21b6f47ca271e7c0defc89be
Sebastian Garcia <Sebasgarse@gmail.com>
2019-09-02 22:56:18 -0400

    ya tengo una base en la que trabajar

--- Pdf Fuser.py
+++ Pdf Fuser.py
@@ -1,3 +1,27 @@
+import os, PyPDF2, docx
 
+def inicio():
+    print("Escriba la direccion de la carpeta")
+    carpeta = input()
+    if os.path.exists(carpeta) and os.path.isdir(carpeta):
+        archivos = [n for n in os.listdir(carpeta) if n.endswith('.jpg') 
+                                                    or n.endswith('.png') 
+                                                    or n.endswith('.jpeg')]
+                                                    
+        #archivo = open(carpeta + '\\' + os.path.basename(carpeta) + '.docx', 'w')
+        #archivo.close()
+        os.chdir(carpeta)
+        doc = docx.Document()
+        doc.add_paragraph('hola')
+        doc.add_page_break()
+        doc.add_picture(archivos[0])
+        doc.save(os.path.basename(carpeta) + '.docx')
+        print(archivos)
+        
+    else:
+        print('Carpeta no existe')
+        inicio()
+        
+        
 if __name__ == '__main__':
-    
+    inicio()
